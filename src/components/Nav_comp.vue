494df99233f61717d8736a2e4492a23ba4f8b9de
PietroDanieli <1pie7d@gmail.com>
2022-10-07 16:04:34 +0200

    create Nav_comp

---
+++ src/components/Nav_comp.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav>
+    <ul class="d_flex gap_1">
+      <li v-for="x in listLink" :key="x">
+        <a :href="x.href">{{ x.label }}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Nav_comp",
+  props: {
+    listLink: Array,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/style/variables.scss";
+
+a {
+  text-decoration: none;
+  color: black;
+}
+</style>
