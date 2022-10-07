44fa8c24d03ab6ec91d5c27f4998f7a6910979c1
PietroDanieli <1pie7d@gmail.com>
2022-10-07 16:01:25 +0200

    prepare data.js file

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const navUtent = [
+  {
+    label: "sign in",
+    active: false,
+    href: "#",
+  },
+  {
+    label: "about us",
+    active: false,
+    href: "#",
+  },
+  {
+    label: "contact us",
+    active: false,
+    href: "#",
+  },
+  {
+    label: "buy now",
+    active: false,
+    href: "#",
+  },
+];
+export const navApp = [
+  {
+    label: "home",
+    active: true,
+    href: "#",
+  },
+  {
+    label: "element",
+    active: false,
+    href: "#",
+  },
+  {
+    label: "features",
+    active: false,
+    href: "#",
+  },
+  {
+    label: "pages",
+    active: false,
+    href: "#",
+  },
+  {
+    label: "portfolio",
+    active: false,
+    href: "#",
+  },
+  {
+    label: "blog",
+    active: false,
+    href: "#",
+  },
+  {
+    label: "shop",
+    active: false,
+    href: "#",
+  },
+];
+export const categoryList = [];
