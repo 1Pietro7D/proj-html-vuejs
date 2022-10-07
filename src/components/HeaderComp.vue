c567225a6282c36158890395489df26c7610c8f8
PietroDanieli <1pie7d@gmail.com>
2022-10-07 16:07:20 +0200

    import data.js and Nav_comp, structure the template with 2 Nav_comp where export with the props the element of data.js

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,14 +1,48 @@
 <template>
-  <header></header>
+  <header>
+    <div id="navUtent_container">
+      <Nav_comp :listLink="navUtent" />
+    </div>
+    <aside id="logo_container">
+      <img src="@/assets/img/logo-default-slim.png" alt="logo header" />
+    </aside>
+    <div id="navApp_container">
+      <Nav_comp :listLink="navApp" />
+    </div>
+  </header>
 </template>
 
 <script>
+import { navUtent } from "@/data.js";
+import { navApp } from "@/data.js";
+import Nav_comp from "./Nav_comp.vue";
 export default {
   name: "HeaderComp",
   props: {},
+  data() {
+    return { navUtent, navApp };
+  },
+  components: {
+    Nav_comp,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 @import "@/assets/style/variables.scss";
+header {
+  div {
+    padding: 1rem 0;
+  }
+  #navUtent_container {
+    background-color: $navUtent_theme;
+  }
+}
+#logo_container {
+  padding: 2rem 0;
+  border-bottom: 2px solid $grey_theme;
+  img {
+    height: 50px;
+  }
+}
 </style>
