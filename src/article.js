cd907680bf8d6ad8690a10b6cc84e7945eb600e3
PietroDanieli <1pie7d@gmail.com>
2022-10-07 16:46:19 +0200

    create article.js with prototipe of  file

---
+++ src/article.js
@@ -0,0 +1,30 @@
+export const post = [
+  { author: "John Down" },
+  { image: "blog-55.jpg" },
+  { date: "10/05/2535 14:15:59" },
+  { categories: ["photografy", "travel"] },
+  { title: "Miao miao bau bau" },
+  {
+    article:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ipsum modi similique, officiis dolor id blanditiis fuga mollitia nisi, iste numquam doloremque recusandae omnis libero reiciendis vero quis accusantium necessitatibus!",
+  },
+  {
+    comments: [
+      {
+        author: "Davide",
+        date: "10/01/2020 15:30:55",
+        message: "è lei?",
+      },
+      {
+        author: "Adriano",
+        date: "10/01/2020 15:32:55",
+        message: "penso di si",
+      },
+      {
+        author: "Fabiola",
+        date: "10/01/2020 15:37:55",
+        message: "si confermo",
+      },
+    ],
+  },
+];
