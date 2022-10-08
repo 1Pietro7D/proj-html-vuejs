28802630897993a1405e071d4decba42b1fc6e70
PietroDanieli <1pie7d@gmail.com>
2022-10-08 20:28:56 +0200

    implement more articles in articles.js

--- src/articles.js
+++ src/articles.js
@@ -2,11 +2,11 @@ export const post = [
   {
     id: 1,
     author: "John Down",
-    image: "blog-54.jpg",
+    image: ["blog-46.jpg"],
     date: "12/01/2022 14:15:59",
-    categories: ["photografy", "travel"],
-    title: "Simple Ways to Have a Pretty Face",
-    status: "popular",
+    categories: ["photografy", "tecnology"],
+    title: "How To Take Better Concert Pictures in 30 Seconds",
+    status: "top",
     article:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ipsum modi similique, officiis dolor id blanditiis fuga mollitia nisi, iste numquam doloremque recusandae omnis libero reiciendis vero quis accusantium necessitatibus!",
     comments: [
@@ -35,10 +35,180 @@ export const post = [
   {
     id: 2,
     author: "John Down",
-    image: "blog-55(1).jpg",
+    image: ["blog-47.jpg"],
+    date: "12/01/2022 14:15:59",
+    categories: ["gadgets", "tecnology"],
+    title: "Gadgets That Make Your Smartphone Even Smarter",
+    status: "top",
+    article:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ipsum modi similique, officiis dolor id blanditiis fuga mollitia nisi, iste numquam doloremque recusandae omnis libero reiciendis vero quis accusantium necessitatibus!",
+    comments: [
+      {
+        author: "Davide",
+        date: "12/01/2022 15:30:55",
+        message: "è lei?",
+      },
+      {
+        author: "Adriano",
+        date: "12/01/2022 15:32:55",
+        message: "penso di si",
+      },
+      {
+        author: "Fabiola",
+        date: "12/01/2022 15:37:55",
+        message: "si confermo",
+      },
+    ],
+  },
+  {
+    id: 3,
+    author: "John Down",
+    image: ["blog-48.jpg"],
+    date: "12/01/2022 14:15:59",
+    categories: ["travel", "photografy"],
+    status: "top",
+    title: "20 Top-Rated Tourist Attractions in Manhattan",
+    article:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ipsum modi similique, officiis dolor id blanditiis fuga mollitia nisi, iste numquam doloremque recusandae omnis libero reiciendis vero quis accusantium necessitatibus!",
+    comments: [
+      {
+        author: "Davide",
+        date: "12/01/2022 15:30:55",
+        message: "è lei?",
+      },
+      {
+        author: "Adriano",
+        date: "12/01/2022 15:32:55",
+        message: "penso di si",
+      },
+      {
+        author: "Fabiola",
+        date: "12/01/2022 15:37:55",
+        message: "si confermo",
+      },
+    ],
+  },
+
+  {
+    id: 5,
+    author: "John Down",
+    image: ["blog-50.jpg"],
+    date: "12/01/2022 14:15:59",
+    categories: ["travel", "photografy"],
+    title: "5 Fun Things to Do at the Beach",
+    status: "top",
+    article:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ipsum modi similique, officiis dolor id blanditiis fuga mollitia nisi, iste numquam doloremque recusandae omnis libero reiciendis vero quis accusantium necessitatibus!",
+    comments: [
+      {
+        author: "Davide",
+        date: "12/01/2022 15:30:55",
+        message: "è lei?",
+      },
+      {
+        author: "Adriano",
+        date: "12/01/2022 15:32:55",
+        message: "penso di si",
+      },
+      {
+        author: "Fabiola",
+        date: "12/01/2022 15:37:55",
+        message: "si confermo",
+      },
+    ],
+  },
+  {
+    id: 6,
+    author: "John Down",
+    image: ["blog-51.jpg"],
     date: "12/01/2022 14:15:59",
     categories: ["photografy", "travel"],
+    status: "top",
+    title: "Amazingly Fresh Fruit And Herb Drinks For Summer",
+    article:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ipsum modi similique, officiis dolor id blanditiis fuga mollitia nisi, iste numquam doloremque recusandae omnis libero reiciendis vero quis accusantium necessitatibus!",
+    comments: [
+      {
+        author: "Davide",
+        date: "12/01/2022 15:30:55",
+        message: "è lei?",
+      },
+      {
+        author: "Adriano",
+        date: "12/01/2022 15:32:55",
+        message: "penso di si",
+      },
+      {
+        author: "Fabiola",
+        date: "12/01/2022 15:37:55",
+        message: "si confermo",
+      },
+    ],
+  },
+  {
+    id: 7,
+    author: "John Down",
+    image: ["blog-55.jpg", "blog-55(1).jpg"],
+    date: "12/01/2022 14:15:59",
+    categories: ["photografy", "travel"],
+    status: "popular",
     title: "Simple Ways to Have a Pretty Face",
+    article:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ipsum modi similique, officiis dolor id blanditiis fuga mollitia nisi, iste numquam doloremque recusandae omnis libero reiciendis vero quis accusantium necessitatibus!",
+    comments: [
+      {
+        author: "Davide",
+        date: "12/01/2022 15:30:55",
+        message: "è lei?",
+      },
+      {
+        author: "Adriano",
+        date: "12/01/2022 15:32:55",
+        message: "penso di si",
+      },
+      {
+        author: "Fabiola",
+        date: "12/01/2022 15:37:55",
+        message: "si confermo",
+      },
+    ],
+  },
+  {
+    id: 8,
+    author: "John Down",
+    image: ["blog-56.jpg", "blog-56(1).jpg"],
+    date: "12/01/2022 14:15:59",
+    categories: ["sports", "business"],
+    status: "top",
+    title: "Simple Ways to Have a Pretty Face",
+    article:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ipsum modi similique, officiis dolor id blanditiis fuga mollitia nisi, iste numquam doloremque recusandae omnis libero reiciendis vero quis accusantium necessitatibus!",
+    comments: [
+      {
+        author: "Davide",
+        date: "12/01/2022 15:30:55",
+        message: "è lei?",
+      },
+      {
+        author: "Adriano",
+        date: "12/01/2022 15:32:55",
+        message: "penso di si",
+      },
+      {
+        author: "Fabiola",
+        date: "12/01/2022 15:37:55",
+        message: "si confermo",
+      },
+    ],
+  },
+  {
+    id: 4,
+    author: "John Down",
+    image: ["blog-49.jpg"],
+    date: "12/01/2022 14:15:59",
+    categories: ["lifestyles", "travel"],
+    status: "top",
+    title: "The Best Way to Ride a Motorcycle",
     article:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ipsum modi similique, officiis dolor id blanditiis fuga mollitia nisi, iste numquam doloremque recusandae omnis libero reiciendis vero quis accusantium necessitatibus!",
     comments: [
