bae4f331f7d646845ae5607dfc95c69596f5f589
PietroDanieli <1pie7d@gmail.com>
2022-10-08 06:37:29 +0200

    prepare second element for articles

--- src/articles.js
+++ src/articles.js
@@ -2,26 +2,58 @@ export const post = [
   {
     id: 1,
     author: "John Down",
-    image: "blog-55.jpg",
-    date: "10/05/2535 14:15:59",
+    image: "blog-54.jpg",
+    date: "12/01/2022 14:15:59",
     categories: ["photografy", "travel"],
-    title: "Miao miao bau bau",
+    title: "Simple Ways to Have a Pretty Face",
     article:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ipsum modi similique, officiis dolor id blanditiis fuga mollitia nisi, iste numquam doloremque recusandae omnis libero reiciendis vero quis accusantium necessitatibus!",
     comments: [
       {
         author: "Davide",
-        date: "10/01/2020 15:30:55",
+        date: "12/01/2022 15:30:55",
+        message: "le conosco",
+      },
+      {
+        author: "Adriano",
+        date: "12/01/2022 15:32:55",
+        message: "sempre loro",
+      },
+      {
+        author: "Fabiola",
+        date: "12/01/2022 15:37:55",
+        message: "fanno finta di essere amiche e poi ti rubano il portafoglio",
+      },
+      {
+        author: "Davide",
+        date: "12/01/2022 15:38:55",
+        message: "mannaggia a loro",
+      },
+    ],
+  },
+  {
+    id: 2,
+    author: "John Down",
+    image: "blog-55(1).jpg",
+    date: "12/01/2022 14:15:59",
+    categories: ["photografy", "travel"],
+    title: "Simple Ways to Have a Pretty Face",
+    article:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ipsum modi similique, officiis dolor id blanditiis fuga mollitia nisi, iste numquam doloremque recusandae omnis libero reiciendis vero quis accusantium necessitatibus!",
+    comments: [
+      {
+        author: "Davide",
+        date: "12/01/2022 15:30:55",
         message: "è lei?",
       },
       {
         author: "Adriano",
-        date: "10/01/2020 15:32:55",
+        date: "12/01/2022 15:32:55",
         message: "penso di si",
       },
       {
         author: "Fabiola",
-        date: "10/01/2020 15:37:55",
+        date: "12/01/2022 15:37:55",
         message: "si confermo",
       },
     ],
