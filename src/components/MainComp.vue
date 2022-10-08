35ad02ece05190d83aa62ab3da617586604cc7e8
PietroDanieli <1pie7d@gmail.com>
2022-10-08 20:42:34 +0200

    create stucture for first section "blog_grid" of main

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,6 +1,12 @@
 <template>
   <main>
-    <div></div>
+    <section class="blog_grid">
+      <div class="max-container">
+        <div v-for="(x, index) in gridBlogElement" :key="index">
+          <p>{{ x.title }}</p>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
@@ -10,11 +16,36 @@ import { categoriesList } from "@/data.js";
 export default {
   name: "MainComp",
   data() {
-    return { post, category: categoriesList };
+    return {
+      post,
+      category: categoriesList,
+      gridBlogElement: [],
+    };
+  },
+  created() {
+    this.getGridBlog(post);
+  },
+  methods: {
+    getGridBlog(post) {
+      for (
+        let index = 0;
+        this.gridBlogElement.length < 6 && index < post.length;
+        index++
+      ) {
+        if (post[index].status == "top") {
+          const element = post[index];
+          this.gridBlogElement.push(element);
+        }
+      }
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
 @import "@/assets/style/variables.scss";
+.blog_grid {
+  padding: 2rem 0;
+  background-color: $grey_theme;
+}
 </style>
