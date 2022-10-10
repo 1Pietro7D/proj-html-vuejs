db26bf0e91457e55cc7793de10c95dff5e8c221f
PietroDanieli <1pie7d@gmail.com>
2022-10-10 12:57:31 +0200

    other condiction for Nav Comp

--- src/components/Nav_comp.vue
+++ src/components/Nav_comp.vue
@@ -1,7 +1,11 @@
 <template>
   <nav>
     <ul class="d_flex gap_1 justify_around">
-      <li v-for="(element, index) in listLink" :key="index">
+      <li
+        :class="{ padXl: paddingXl == true }"
+        v-for="(element, index) in listLink"
+        :key="index"
+      >
         <a :href="element.href" :class="{ txWhite: txWhite == true }">{{
           element.label
         }}</a>
@@ -16,17 +20,28 @@ export default {
   props: {
     listLink: Array,
     txWhite: Boolean,
+    paddingXl: Boolean,
   },
 };
 </script>
 
 <style lang="scss" scoped>
+@import "@/assets/style/variables.scss";
+li {
+  display: inline-block;
+}
 a {
   text-decoration: none;
   color: black;
   text-transform: uppercase;
   &.txWhite {
     color: white;
+  }
+}
+.padXl {
+  > a {
+    padding: 0.5rem 2rem;
+    background-color: $paddin_dark_theme;
   }
 }
 </style>
